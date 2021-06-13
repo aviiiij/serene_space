96e304c47090e5e2270cd0d2d749c0b99dcffdb5
aviiii <avi.j@somaiya.edu>
2021-06-13 19:37:00 +0530

    Add Navbar

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Vii's Space", url=os.getenv("URL"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
