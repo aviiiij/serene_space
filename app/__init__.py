1e666a877d34609825684c37ef4f7d85729251d7
aviiii <avi.j@somaiya.edu>
2021-06-18 19:53:56 +0530

    Add Health Endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="Vii's Space", url=os.getenv("URL"))
 
+@app.route('/health')
+def health():
+    return 'Hello, This is a Health Check'
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
