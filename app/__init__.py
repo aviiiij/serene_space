6dc1c3051822284fcd6e109179b260950e1f5fb8
Cloud User <centos@ip-172-31-37-230.ap-south-1.compute.internal>
2021-06-23 12:44:28 +0000

    Add Sqlite Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
+from . import db
 
 load_dotenv()
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title="Vii's Space", url=os.getenv("URL"))
+
 
 @app.route('/health')
 def health():
@@ -16,4 +20,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
